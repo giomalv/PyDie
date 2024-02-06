5b9da04785e33879af3e25b89efac29ce98e6995
Giorgio <giorgio@malv.one>
2024-02-06 10:10:36 +0000

    Added basic icon for application

--- dieQt.py
+++ dieQt.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QComboBox, QLineEdit, QListWidget
 from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
 import sys
 import random
 import winsound as ws
@@ -7,7 +8,7 @@ import winsound as ws
 class PyDie(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.setWindowIcon(QtGui.QIcon('resources/icon.ico'))
         self.die_values = {"D100":100, "D20": 20, "D12": 12, "D10": 10, "D8": 8, "D6": 6, "D4": 4, "Coin Flip":2, "Custom": 1}
         self.initUI()
 
